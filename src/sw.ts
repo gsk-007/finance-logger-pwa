977831ea099bd19dce142e98e96094144d978193
Ronny <gsingh16742@gmail.com>
2023-07-22 17:26:59 +0530

    offline access

---
+++ src/sw.ts
@@ -0,0 +1,22 @@
+const assets = [
+  "/",
+  "app.js",
+  "styles.css",
+  "sw-register.js",
+  "interfaces/*",
+  "classes/*",
+]
+
+self.addEventListener("fetch", (event: any) => {
+  event.respondWith(
+    caches.match(event.request).then((response: any) => {
+      const fetchPromise = fetch(event.request).then((networkResponse) => {
+        caches.open("assets").then((cache) => {
+          cache.put(event.request, networkResponse.clone())
+          return networkResponse
+        })
+      })
+      return response || fetchPromise
+    })
+  )
+})
